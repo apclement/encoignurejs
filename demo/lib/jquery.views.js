1617aac25b8a04b6ce22bc2e4fc7098b18986ff5
Alexandre P. Clement <a.p.clement@gmail.com>
2012-10-10 16:55:54 +0200

    update

--- demo/lib/jquery.views.js
+++ demo/lib/jquery.views.js
@@ -72,8 +72,8 @@ $.fn.view = function() {
 		$view.rebind(endEvents, endHandler).addClass(visibleClass).removeClass(effectClass + ' out').addClass(effect+' in')
 		
 		switching.done(function(){
-			$view.removeClass('visible').removeClass(effectClass)									
-			otherViews.removeClass('visible').removeClass(effectClass)									
+			$view.removeClass('visible')//.removeClass(effectClass)									
+			otherViews.removeClass('visible')//.removeClass(effectClass)									
 		});
 		
 		if (effect == 'none'){
