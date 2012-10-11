f5f98a0423313e2900f73d973b621a457c524ed2
Alexandre P. Clement <a.p.clement@gmail.com>
2012-10-11 21:42:56 +0200

    update

--- demo/people/people.js
+++ demo/people/people.js
@@ -39,7 +39,7 @@ App.controllers.people = {
 		$('#viewholder').loadOnce('people/people.html', function(){			
 			that.buildList()			
 			
-			$('#peopledetails').find(':input').attr('disabled', true).blur()		
+			$('#peopledetails').find(':input').attr('disabled', true)	
 						
 			$('#people').view()
 			$('#peoplelistview').view()			
@@ -58,7 +58,7 @@ App.controllers.people = {
 			$('#people').view()
 			$('#peopledetails').view()
 			
-			$('#peopledetails').find(':input').attr('disabled', false).first().focus()	
+			$('#peopledetails').find(':input').removeAttr('disabled')	
 		});		
 	}
 };
