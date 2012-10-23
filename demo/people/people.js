0ad6084865e35d19df24ab5f0bd17d017795d765
Alexandre P. Clement <a.p.clement@gmail.com>
2012-10-23 13:28:44 +0200

    update

--- demo/people/people.js
+++ demo/people/people.js
@@ -78,8 +78,10 @@ $(document).on('init', '#peopledetails', function(){
 
 $(document).on('init', '#peoplelistview', function(){
 	var $view = $(this)
-	$view.on('click', 'a', function(){
-		//$view.find('a').data('href', function(){ return $(this).attr('href') }).removeAttr('href')
+	$view.on('click', 'a', function(e){
+		e.preventDefault()
+		window.location.hash = $(this).attr('href')
+		
 	})
 })
 
