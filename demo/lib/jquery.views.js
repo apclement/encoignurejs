0f5cbe1e99fc52f1f75e34272f5012b0b24c5f58
Alexandre P. Clement <a.p.clement@gmail.com>
2012-10-26 13:08:18 +0200

    update

--- demo/lib/jquery.views.js
+++ demo/lib/jquery.views.js
@@ -114,7 +114,9 @@ $.fn.loadOnce = function(url, cb){
 			views.trigger('init')			
 			cb()
 			 window.scrollTo( 0, 1 );
-			$(window).resize() 
+			setTimeout(function(){ 
+				$(window).resize() 
+			}, 500);
 		});	
 	} else {			
 		cb()
