cb822091a0d988eb6b4fa8f985e90edda00fdaf8
Alexandre P. Clement <a.p.clement@gmail.com>
2012-11-02 12:24:48 +0100

    update

--- demo/demo.js
+++ demo/demo.js
@@ -82,15 +82,13 @@ $(window).resize(function(){
 })   
 
 function hideAddressBar(){
-  if(!window.location.hash)
-  {
       if(document.height < window.outerHeight)
       {
           document.body.style.height = (window.outerHeight + 50) + 'px';
       }
 
       setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
-  }
+  
 }
 
 $(document).load(function(){     
