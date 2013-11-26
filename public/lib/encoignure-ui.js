064e89ec1b15a96be23cdaf6fe145a6e025b4430
Alexandre P. Clement <a.p.clement@gmail.com>
2013-11-26 10:12:39 +0100

    update

---
+++ public/lib/encoignure-ui.js
@@ -0,0 +1,42 @@
+define(['jquery', 'watch', 'weld'], function($, WatchJS) {
+
+	// weld jquery plugin
+	$.fn.weld = function (data, config) {		
+		weld(this[0], data, config);	
+		return this
+	};
+	
+	$.fn.itemlist = function(items, config){
+		var $this = $(this)
+				
+		$this.data('items', items)		
+		
+		function render(){
+			$this.find('li').removeClass('leave').weld(items, config)
+			
+			$this.find('li').each(function(i){
+				$(this).data('item', items[i])
+			});			
+		}		
+		
+		WatchJS.watch({value: items}, function(name, action, params){
+			console.info('watch: '+ action + ' '+ name + ' '+ params)
+			
+			var removed = $this.find('li').filter(function(){
+				return items.indexOf($(this).data('item')) == -1;
+			}).addClass('leave');
+					
+			if (removed.length > 0){
+				setTimeout(render, 1000)
+			} else {
+				render()
+			}
+		});	
+
+		render()
+		
+		return $this;
+	}	
+	
+	return true;
+});
